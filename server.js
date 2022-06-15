ff801abb8c593626cde3d5d435120522ff1fa660
Dmitry_Lyubimov <Dmitry_Lyubimov@epam.com>
2022-06-15 17:05:52 +0300

    proper config for heroku deployment

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const app = express()
-const port = 3000
+
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static(__dirname + '/public'))
 
 app.get('/lifecheck', (req, res) => {
-  res.send('Hello World!')
+  res.send('Pretty Healthy so far!')
 })
 
-app.use(express.static('public'))
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.listen(app.get('port'), () => {
+  console.log(`The App listening on port ${app.get('port')}`)
 })
